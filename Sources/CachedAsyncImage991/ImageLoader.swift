db996671b7b0cc3466f3a6b867bed02baeb7f428
Oleg991 <o.n.eremenko@gmail.com>
2023-12-10 12:18:53 +0300

    Added OSLog

--- Sources/CachedAsyncImage991/ImageLoader.swift
+++ Sources/CachedAsyncImage991/ImageLoader.swift
@@ -1,7 +1,9 @@
 import Foundation
 import UIKit.UIImage
+import OSLog
 
 final class ImageLoader: ObservableObject {
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ImageLoader")
     @Published private(set) var image: UIImage?
     @Published private(set) var isLoading = false
     private let url: URL?
@@ -11,16 +13,33 @@ final class ImageLoader: ObservableObject {
 
     @MainActor
     func load() async {
-        guard let url, !isLoading else { return }
+        guard let url, !isLoading else {
+            logger.debug("Skipping this call to `load()` method")
+            return
+        }
         if let image = cache[url], self.image != image {
+            logger.debug("Using a cached image for url: \(url, privacy: .public)")
             self.image = image
             return
         }
         isLoading = true
-        if let (data, _) = try? await URLSession.shared.data(from: url),
-           let loadedImage = UIImage(data: data) {
-            cache[url] = loadedImage
-            self.image = loadedImage
+        logger.debug("Loading an image from url: \(url, privacy: .public)")
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let loadedImage = UIImage(data: data) {
+                cache[url] = loadedImage
+                self.image = loadedImage
+                logger.debug("Successfully got an image from url: \(url, privacy: .public)")
+            } else {
+                logger.error("Failed to create a UIImage with data")
+            }
+        } catch {
+            logger.error(
+                """
+                Failed to load image from URL: \(url, privacy: .public)
+                Error: \(error.localizedDescription, privacy: .public)
+                """
+            )
         }
         isLoading = false
     }
